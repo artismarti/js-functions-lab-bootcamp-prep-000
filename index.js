fe26a7b1b2f919e2d6f845463632168dfa55fda4
Arti M <artismarti@gmail.com>
2018-08-30 09:59:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,14 +11,7 @@ function happyHolidayTo(holiday, name) {
 
 
 /*
-
-  describe('happyHolidayTo(holiday, name)', () => {
-    it('returns "Happy ${holiday}, ${name}!"', () => {
-      expect(happyHolidayTo('Independence Day', 'you')).toEqual('Happy Independence Day, you!')
-    })
-  })
-
-  describe('holidayCountdown(holiday, days)', () => {
+ describe('holidayCountdown(holiday, days)', () => {
     it('returns "It\'s ${days} days until ${holiday}!"', () => {
       expect(holidayCountdown("Mother's Day", 20)).toEqual("It's 20 days until Mother's Day!")
     })
